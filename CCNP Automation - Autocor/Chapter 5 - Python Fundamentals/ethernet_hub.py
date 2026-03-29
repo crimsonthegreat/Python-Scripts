a462c0fe3af792c2292375e82d01a4947c528b86
crimsonthegreat <jpschaffran1@gmail.com>
2026-03-29 15:48:22 -0700

    CCNP Automation - Autocor: Chapter 5 Arp Cache

--- CCNP Automation - Autocor/Chapter 5 - Python Fundamentals/ethernet_hub.py
+++ CCNP Automation - Autocor/Chapter 5 - Python Fundamentals/ethernet_hub.py
@@ -11,23 +11,25 @@ Please wait
 """
 print(boot_message)
 
-print(hubports[0])
-print(hubports[1])
-print(hubports[2])
-print(hubports[3])
+for hubport in hubports:
+    print(hubport)
 
 while True:
-    port = input("Generate signal into port: ")
-    port_number = int(port)
-    if port_number < len(hubports) and port_number >= 0:
-        ingress_port = hubports[port_number]
-        print("⚡️ Receiving signal on " + ingress_port)
-    else:
-        print("❌ Invalid entry: " + port)
-        exit()
-
-    for egress_port in hubports:
-        if egress_port == ingress_port:
-            print("🔴 Not transmitting on port " + egress_port)
+    try:
+        port = input("Generate signal into port: ")
+        port_number = int(port)
+        if port_number < len(hubports) and port_number >= 0:
+            ingress_port = hubports[port_number]
+            print("⚡️ Receiving signal on " + ingress_port)
         else:
-            print("🟢 Transmitting on port " + egress_port)
+            print("❌ Invalid entry: " + port)
+            break
+
+        for egress_port in hubports:
+            if egress_port == ingress_port:
+                print("🔴 Not transmitting on port " + egress_port)
+            else:
+                print("🟢 Transmitting on port " + egress_port)
+                
+    except ValueError:
+        print("❌ You must enter a number!")
