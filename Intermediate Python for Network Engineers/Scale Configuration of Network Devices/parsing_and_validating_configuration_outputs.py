31e5f587bf9662a97810fb474e4a2b4daa98692d
crimsonthegreat <jpschaffran1@gmail.com>
2026-04-11 16:46:42 -0700

    IPYNE: Added Comment to Parsing and validating configuration outputs

--- Intermediate Python for Network Engineers/Scale Configuration of Network Devices/parsing_and_validating_configuration_outputs.py
+++ Intermediate Python for Network Engineers/Scale Configuration of Network Devices/parsing_and_validating_configuration_outputs.py
@@ -19,7 +19,15 @@ try:
         print("Interface is up and operational")
     else:
         print("Interface is down or has issues")
-        
+
 finally:
     device.disconnect()
 
+"""
+If you are taking IPYNE on Cisco U and following along, their code does not work and will return errors.
+The above code works and is tested against a virtual router in CML.  The with portion of the Cisco U code returns
+error 'TypeError: 'Device' object is not callable' meaning the device is not able to be used as a context object
+and instead needs to be a hard coded connect and disconnect.
+Additionally, the intf section does not work because the parse stores all of the interfaces under the key interface,
+which has to be pulled first and then you can get the specific interface info you are looking for.
+"""
