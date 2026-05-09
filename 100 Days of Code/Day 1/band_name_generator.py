f2418dab07c96659a242ff2734ac9581d480ba34
crimsonthegreat <jpschaffran1@gmail.com>
2026-05-09 13:14:15 -0400

    100 Days of Code: Day 1 Optimized

--- 100 Days of Code/Day 1/band_name_generator.py
+++ 100 Days of Code/Day 1/band_name_generator.py
@@ -1,3 +1,10 @@
+# Function to quit the loop
+def break_loop(user_input):
+    if user_input == 'q' or user_input == 'Q':
+        quit()
+    else:
+        pass
+
 print("Band Name Generator")
 
 # While Loop for generating a Band Name based on place of birth and favorite pet
@@ -5,26 +12,23 @@ while True:
     print("Let's generate a band name for you!")
     print("Type q at any time to exit...")
 
-# Get user input for place of birth
+    # Get user input for place of birth
     born = input("Where were you born: ")
 
-# Check if the user wants to quit by checking for q or Q
-    if born == 'q' or born == 'Q':
-        break
-    else:
-        pass
+    # Check if the user wants to quit by checking for q or Q
+    break_loop(born)
 
-# Get user input on favorite pet
+    # Get user input on favorite pet
     pet = input("What was your favor pet growing up: ")
 
-# Check if the user wants to quit by checking for q or Q
-    if pet == 'q' or pet == 'Q':
-        break
-    else:
-        print("Recommended band name:")
-        print(f"{born} {pet}")
+    # Check if the user wants to quit by checking for q or Q
+    break_loop(pet)
 
-# While Loop to see if the users wants to generate additional band name
+    # Output recommend band name
+    print("Recommended band name:")
+    print(f"{born} {pet}")
+
+    # While Loop to see if the users wants to generate additional band name
     while True:
         leave = input("Would you like to generate another name: y or n: ")
 
